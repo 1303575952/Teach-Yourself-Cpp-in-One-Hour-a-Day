7324b5d2032b8fc127e506359f7c05b96a0c9884
1303575952@qq.com <1303575952@qq.com>
2019-04-28 16:21:42 +0800

    chapter9

---
+++ chapter9/9-6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Human {
+private:
+    int age;
+    string name;
+
+public:
+    Human(string humansName = "Adam", int humansAge = 25)
+            : name(humansName), age(humansAge) {
+        cout << "Constructed a human called " << name;
+        cout << ", " << age << " years old" << endl;
+    }
+};
+
+int main() {
+    Human adam;
+    Human eve("Eve", 18);
+
+    return 0;
+}
